b4e67c9043b25f6524000b8e316f08cbcdbb5c11
Cory <coryrosser@gmail.com>
2020-11-10 15:03:46 -0500

    header work

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,3 +1,15 @@
 import React from 'react'
 import {ListGroup} from 'react-bootstrap'
 import styled from 'styled-components'
+
+const Styles = styled.div`
+`
+
+
+const Name = (props) => {
+ return (
+
+ )
+}
+
+export default Name
