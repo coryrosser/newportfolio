acdc5a30fd6ac57e576fd0220ed9c0853298a4e0
Cory <coryrosser@gmail.com>
2020-11-10 19:47:03 -0500

    honeycomb

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -10,18 +10,28 @@ const Styles = styled.div`
 const Blogs = (props) => {
   return (
     <Styles>
-      <Row className='picker-row'>
-        <Col xs={6}>
-         <p 
-         onClick={() => {props.handleViewChange(0)}}
-         className={props.view === 0 ? 'picker-item active-item' : 'picker-item'}>Projects</p>
-        </Col>
-        <Col xs={6}>
-        <p 
-        onClick={() => {props.handleViewChange(1)}}
-        className={props.view === 1 ? 'picker-item active-item' : 'picker-item'}>Blogs</p>
-        </Col>
-      </Row>
+      <blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/local-storage-what-when-and-how-fb73c746391">Local Storage: What, When, and How?</a></h4><p>localStorage is a part of the HTML5 Web Storage API. It allows you, the developer, the ability to store information within the browser and gives you a way to easily retrieve and use the stored data...</p></blockquote>
+
+
+
+<blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/a-programmers-silent-assistant-85144c759b67">A Programmers Silent Assistant</a></h4><p>Rails supports six types of associations: In my example above we used a has_many association and belongs_to which, thus far, are more commonly used by me along with has_many :though . I won't get into all of the associations in this post but I will try to explain in a little more detail the two we've already used and how they help us.</p></blockquote>
+
+
+      <blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/structure-of-the-flow-ad82149a846">Structure of the Flow</a></h4><p>Understanding the structure and information flow of an Angular application Angular has become one of the most widely used solutions to large scale single-page web app lications on the market. Starting in 2010, Google released the first open-source version of Angular (known as AngularJS), and over the next few years, the team at Google would completely redesign and rebuild Angular into what we know now as Angular 2+.</p></blockquote>
+      <blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/css-grid-basics-84b51e66da4f">CSS Grid Basics</a></h4><p>CSS Grid Layout brings a two-dimensional grid system to CSS. Grids can be used to layout major page areas or small user interface elements. This is similar to the Column and Row setup that many Bootstrap users are familiar with. In this blog, I'll go over the basics of the CSS grid and how to begin familiarizing yourself with it.</p></blockquote>
+      <blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/a-react-developers-take-on-angular-ab2a376332c0">A React Developer's Take on Angular</a></h4><p>A summary of my experience learning Angular basics As a developer who graduated from Flatiron School, I was exposed to the same technology many, if not all, other Software Engineering students were. This includes work with Ruby, Rails, JavaScript, and React.</p></blockquote>
+
+      <blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/relational-vs-non-relational-databases-9bc00915b724">Relational vs Non-Relational Databases</a></h4><p>What's the difference, and why is this good to know? When I set out to learn about Software Engineering, I was introduced to working with persis tent data early on. The first Database Management System I worked with was SQLite3, before moving onto PostgreSQL.</p></blockquote>
+
+
+<blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/balancing-your-growth-as-a-developer-e76bc6e8c1ba">Balancing your growth as a developer</a></h4><p>As a developer who's recently graduated from a boot-camp and looking to start my career, I often find my self spending countless hours on side-projects. Everything from full-stack feature projects to small scale front-end React Apps.</p></blockquote>
+
+
+<blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/yes-your-color-scheme-is-important-8ed7d189ed">Yes, Your Color Scheme is Important.</a></h4><p>An introduction Color Theory and User Psychology Have you ever stopped and wondered why so many Social Media sites use some variation of Blue a s their primary color? If not then now you have.</p></blockquote>
+
+
+<blockquote class="embedly-card" data-card-theme="dark"><h4><a href="https://coryrosser.medium.com/becoming-a-problem-solver-73ac10425764">Becoming a Problem Solver</a></h4><p>If you've ever been plugging away on a project and hit "Run" on your infallible block of code only to see the console have a melt-down; just know, everyone has.</p></blockquote>
+
     </Styles>
   )
 }
