8813742cbcdd9d213d809c5113aaeba5d73d61bf
Cory <coryrosser@gmail.com>
2020-11-12 15:28:56 -0500

    honeycomb

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,38 @@ import {Row, Col} from 'react-bootstrap'
 import styled from 'styled-components'
 
 const Styles = styled.div`
-  color: white;
+  .about-row {
+    color: white;
+    border: white solid 1px;
+    width: 100%;
+    height: 50vh;
+  }
+  .skills-col {
+    
+    border: white solid 1px;
+  }
+  .about-col {
+    
+    border: red solid 1px;
+  }
 `
 
 const About = () => {
   return (
     <Styles>
-      <h1>About me</h1>
+      <Row className='about-row'>
+        <Col className='skills-col'>
+          <Row className='skills-title-row'>
+
+          </Row>
+        </Col>
+
+        <Col className='about-col'>
+          <Row className='about-title-row'>
+
+          </Row>
+        </Col>
+      </Row>
     </Styles>
   )
 }
